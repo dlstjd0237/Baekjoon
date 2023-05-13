5555164504f4906ceaca0a019506e9731d6332a6
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-05-13 11:22:34 +0900

    Update 알람 시계.cs

--- 백준/Bronze/2884. 알람 시계/알람 시계.cs
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cs
@@ -6,10 +6,10 @@ namespace Back
     {
         static void Main(string[] args)
         {
-            string dkdlt = Console.ReadLine();
-            string[] tltvkf = dkdlt.Split();
-            int a = int.Parse(tltvkf[0]);
-            int b = int.Parse(tltvkf[1]);
+            string qq = Console.ReadLine();
+            string[] ww = qq.Split();
+            int a = int.Parse(ww[0]);
+            int b = int.Parse(ww[1]);
             if (b - 45<0)
             {
                 b = b + 60 - 45;
