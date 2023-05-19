4160f4885b410235babaf76c27bbdb1c20ffd7ab
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-05-19 16:52:30 +0900

    [Bronze V] Title: We love kriii, Time: 48 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Text;
+
+namespace Back
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("강한친구 대한육군");
+            }
+        }
+    }
+}
