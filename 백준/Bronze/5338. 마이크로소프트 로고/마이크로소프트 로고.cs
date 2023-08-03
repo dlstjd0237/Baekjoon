cb8e9cfdd41dc2ebd668879241a93cebafdb3fb2
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-08-03 17:36:52 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 52 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Back
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+        }
+    }
+}
