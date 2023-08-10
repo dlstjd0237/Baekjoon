429ba05354b71fb31ad3abe1c51aab904d4f4fd1
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-08-10 14:55:01 +0900

    [Bronze V] Title: A+B, Time: 72 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Bronze/26711. A＋B/A＋B.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Numerics;
+namespace Baek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(a+ BigInteger.Parse(Console.ReadLine()));
+        }
+    }
+}
