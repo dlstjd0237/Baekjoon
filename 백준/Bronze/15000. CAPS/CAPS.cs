9b40d2e24be0f96abcde5c8a6169e776f5c582e8
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-08-10 14:41:38 +0900

    [Bronze V] Title: CAPS, Time: 292 ms, Memory: 12436 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Baek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().ToUpper());
+        }
+    }
+}
