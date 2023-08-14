95b27f8710e20bebc0791922b40c96963a2705f7
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-08-14 11:00:00 +0000

    [Bronze V] Title: 홍익대학교, Time: 84 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Baek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine())-1946);
+        }
+    }
+}
