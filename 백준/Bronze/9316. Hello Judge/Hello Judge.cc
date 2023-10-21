50ecbbca4c29acdf32215fde11da4e9aa5bfc4c5
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2023-10-21 19:59:19 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main()
+{
+    int input;
+    std::cin >> input;
+    for (size_t i = 1; i <= input; i++)
+    {
+        std::cout << "Hello World, Judge "<< i << "!\n";
+    }
+}
+
