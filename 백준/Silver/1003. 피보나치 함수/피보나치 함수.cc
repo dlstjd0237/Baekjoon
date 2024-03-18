724325a3b4aca5b90ecaebc5582a37904b6cc6e1
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2024-03-19 01:24:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
 	int count, num;
-	int F[41] = { 0,1 };
+	int F[42] = { 1,0,1 };
 
 
-	for (int i = 2; i <= 40; ++i)
+	for (int i = 3; i <= 41; ++i)
 	{
 		F[i] = F[i - 1] + F[i - 2];
 	}
@@ -17,14 +17,7 @@ int main()
 	for (int i = 0; i < count; i++)
 	{
 		cin >> num;
-		if (num < 1) {
 
-			cout << 1 << " " << 0 << "\n";
-		}
-		else
-		{
-			cout << F[num - 1] << " " << F[num] << "\n";
-
-		}
+		cout << F[num] << " " << F[num + 1] << "\n";
 	}
 }
