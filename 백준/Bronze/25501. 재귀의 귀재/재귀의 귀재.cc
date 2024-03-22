39c09b40f03910a601073cacd58483a0f642181a
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2024-03-22 10:56:46 +0900

    Update 재귀의 귀재.cc

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,32 +1,29 @@
-	
 #include <iostream>
 #include <string.h>
-
 int recursion(const char* s, int l, int r, int& n) {
-	if (l >= r) return 1;
+	++n;
+	if (l >= r)  return 1;
 	else if (s[l] != s[r]) return 0;
-	else { n++; return recursion(s, l + 1, r - 1, n); }
+	else  return recursion(s, l + 1, r - 1, n);
 }
-
 int isPalindrome(const char* s, int& n) {
 	return recursion(s, 0, strlen(s) - 1, n);
 }
 int main(){
-		int input;
+	int input;
 	std::cin >> input;
-	int c[1001];
-	int b[1001];
+	int result[1001];
+	int count[1001];
 	for (int i = 0; i < input; ++i)
 	{
 		char a[1001];
-		b[i] = 0;
+		count[i] = 0;
 		std::cin >> a;
-		c[i] = isPalindrome(a, b[i]);
+		result[i] = isPalindrome(a, count[i]);
 
 	}
-
 	for (int i = 0; i < input; ++i)
 	{
-		std::cout << c[i] << " " << ++b[i] << std::endl;
+		std::cout << result[i] << " " << count[i] << std::endl;
 	}
 }
