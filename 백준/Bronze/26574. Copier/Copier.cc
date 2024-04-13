291a30106525b39e9b100d4e6841f786c694eee9
dlstjd0237 <123615243+dlstjd0237@users.noreply.github.com>
2024-04-13 23:41:31 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int count;
+	cin >> count;
+
+	for (int i = 0; i < count; ++i)
+	{
+		int input;
+		cin >> input;
+		cout << input << " " << input << "\n";
+	}
+}
