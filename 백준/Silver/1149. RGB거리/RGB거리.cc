20d8c59f522d6ce498a9150b2b939f81fd328181
백인성 <123615243+dlstjd0237@users.noreply.github.com>
2025-04-16 09:59:29 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int dp[1005][3];
-
+int dp[1000][3];
 int main()
 {
 	cin.tie()->sync_with_stdio(false);
-
 	int n, sum;
 	cin >> n;
-	for (short i = 0; i < n; ++i)
-		for (short j = 0; j < 3; ++j)
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < 3; ++j)
 			cin >> dp[i][j];
 
-	for (short i = 1; i < n; ++i) {
+	for (int i = 1; i < n; ++i) {
+
 		dp[i][0] += min(dp[i - 1][1], dp[i - 1][2]);
 		dp[i][1] += min(dp[i - 1][0], dp[i - 1][2]);
 		dp[i][2] += min(dp[i - 1][1], dp[i - 1][0]);
 	}
 
-	sum = min({ dp[n - 1][0],dp[n - 1][1],dp[n - 1][2] });
+	sum = min({ dp[n - 1][0], dp[n - 1][1],dp[n - 1][2] });
 
 	cout << sum;
-
 
 	return 0;
 }
